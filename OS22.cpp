6d185e8b2833964547e739e1f028135ad9936a0b
R.velan <velan1087@gmail.com>
2025-03-26 09:57:52 +0530

    Add files via upload

---
+++ OS22.cpp
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+void bestFit(int blockSize[], int m, int processSize[], int n) {
+    int allocation[n];
+    for (int i = 0; i < n; i++) allocation[i] = -1;
+
+    for (int i = 0; i < n; i++) {
+        int bestIdx = -1;
+        for (int j = 0; j < m; j++) {
+            if (blockSize[j] >= processSize[i]) {
+                if (bestIdx == -1 || blockSize[bestIdx] > blockSize[j])
+                    bestIdx = j;
+            }
+        }
+        if (bestIdx != -1) {
+            allocation[i] = bestIdx;
+            blockSize[bestIdx] -= processSize[i];
+        }
+    }
+
+    printf("Process No.\tBlock No.\n");
+    for (int i = 0; i < n; i++) {
+        if (allocation[i] != -1)
+            printf("%d\t\t%d\n", i + 1, allocation[i] + 1);
+        else
+            printf("%d\t\tNot Allocated\n", i + 1);
+    }
+}
+
+int main() {
+    int blockSize[] = {100, 500, 200, 300, 600};
+    int processSize[] = {212, 417, 112, 426};
+    int m = sizeof(blockSize) / sizeof(blockSize[0]);
+    int n = sizeof(processSize) / sizeof(processSize[0]);
+    bestFit(blockSize, m, processSize, n);
+    return 0;
+}
